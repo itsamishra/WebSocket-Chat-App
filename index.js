9964928da9db323143bf10ad796f677f90abd36d
Ash <ashutoshmishra413@gmail.com>
2019-03-08 11:13:44 -0800

    Added start script

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var path = require('path');
 var favicon = require('serve-favicon');
+var port = process.env.PORT || 3000;
 
 var app = express()
 app.use(favicon(path.join(__dirname, 'images', 'icon.jpg')));
@@ -10,4 +11,4 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 })
 
-app.listen(3000)
+app.listen(port)
