213fe040e2057793c8b41e79fbad92a3b2ef9237
Ash <ashutoshmishra413@gmail.com>
2019-03-13 14:51:37 -0700

    Moved text validation from client -> server

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 'use strict';
 
+
 // Imports
 const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
+
+
+// Functions
+// Checks for restricted keywords
+function isRestricted(messageStr) {
+    messageStr = messageStr.toLowerCase();
+
+    for (let i = 0; i < RESTRICTED_KEYWORDS.length; i++) {
+        if (messageStr.indexOf(RESTRICTED_KEYWORDS[i]) !== -1) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+// Constants
+// Array of restricted keywords
+const RESTRICTED_KEYWORDS = [
+    'script',
+    '<',
+    '>',
+    '/',
+    '\\',
+    'alert',
+    'console'
+];
+
 
 // Gets port & index file
 const PORT = process.env.PORT || 3000;
@@ -28,7 +58,10 @@ io.on('connection', (socket) => {
 
     // ... listens for message to server
     socket.on('send_message_to_server', (data) => {
-        io.sockets.emit('send_message_to_client', data);
+        // Makes sure message has no restricted keywords
+        if (!isRestricted(data["message"].trim()) && data["message"].trim() != '') {
+            io.sockets.emit('send_message_to_client', data);
+        }
     });
 
     // Upon disconnection, prints prompt
